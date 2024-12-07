a1dfcb125747ea0a424b305130b7a1537b289479
YetonDew <ariasjefferson@hotmail.com>
2024-12-07 17:44:37 +0100

    TexExample margin

--- src/components/TexExample.jsx
+++ src/components/TexExample.jsx
@@ -1,6 +1,6 @@
 function TexExample() {
     return (
-        <section className="py-10 bg-white sm:py-16 lg:py-24">
+    <section className="mt-[-100px] py-10 bg-white sm:py-16 lg:py-24">
     <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div className="max-w-2xl mx-auto text-center">
             <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">How does it work?</h2>
