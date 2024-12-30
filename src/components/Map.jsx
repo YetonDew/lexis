3f0a23ed47d26b24eda636cc3aafe388b76a36ad
YetonDew <ariasjefferson@hotmail.com>
2024-12-30 16:49:55 +0100

    removing old landing version adding custom marker and layer to the map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,21 +4,38 @@ import "leaflet/dist/leaflet.css";
 
 export default function MapComponent(props) {
   const _ref = useRef();
+  const TILE_LAYER_URL = "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png";
+  const ATRIBUTION = 'Map data &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" title="OpenStreetMap">OpenStreetMap</a>, Imagery &copy; <a href="https://carto.com/" target="_blank" title="Carto">Carto</a>';
 
   useEffect(() => {
-    if(!_ref.current){
-        _ref.current = L.map("map", {
-            center: [props.lat, props.lng],
-            zoom: 20,
-          });
-          L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(
-            _ref.current
-          );
-          L.marker([props.lat, props.lng])
-            .bindPopup("Biuro Lexis")
-            .openPopup()
-            .addTo(_ref.current);
+    if (!_ref.current) {
+      // init map
+      _ref.current = L.map("map", {
+        center: [props.lat, props.lng],
+        zoom: 18,
+      });
+
+      // Use layer of carto 
+      L.tileLayer(TILE_LAYER_URL, {
+        attribution: ATRIBUTION,
+        subdomains: "abcd", 
+        maxZoom: 19,
+      }).addTo(_ref.current);
+
+      const customIconMarker = L.icon({
+        iconUrl: "/lexisMapMarker.png",
+        iconSize: [44, 67], // Icon size
+        iconAnchor: [19, 57], // Anchor point
+        popupAnchor: [0, -59], // Popup anchor
+      });
+
+      // add marker
+      L.marker([props.lat, props.lng ], { icon: customIconMarker })
+        .bindPopup("Biuro Lexis")
+        .openPopup()
+        .addTo(_ref.current);
     }
-  }, []);
+  }, [props.lat, props.lng]); // add props.lat and props.lng as dependencies
+
   return <div className="h-[300px] z-20" id="map"></div>;
 }
