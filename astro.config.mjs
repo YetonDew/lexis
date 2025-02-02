39bbc87c4968458a767807d827676bcc0ea5c127
develop-iro <iro.developer@hotmail.com>
2025-02-02 16:58:47 +0100

    feat: apply updgrade tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [ react()]
 });
